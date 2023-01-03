33d30136ee3f4f4a8dea23970d8d6fc4b9087355
Gagan Gowda <gagangowda26@gmail.com>
2023-01-02 22:46:18 -0800

    Started TCP server and listener from main.go

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
 	fmt.Println("TCP Chat Server - WIP")
+	serv := newServer()
+
+	listener, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("Can't start server - %s", err.Error())
+	}
+
+	defer listener.Close()
+	log.Printf("Started server on :8888")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Unable to accept connection.")
+			continue
+		}
+
+		newClient := serv.newClient(conn)
+		go newClient.readInput()
+	}
 }
