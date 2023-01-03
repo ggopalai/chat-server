572dfbb6e5bdc765df003cd6b465eb7e0bf3b943
Gagan Gowda <gagangowda26@gmail.com>
2023-01-02 22:45:41 -0800

    Defined read input method for client

--- client.go
+++ client.go
@@ -1,10 +1,71 @@
 package main
 
-import "net"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
 
 type client struct {
 	conn        net.Conn
 	name        string
 	room        *room
 	commandChan chan<- command
+}
+
+func (c *client) readInput() {
+	for {
+		msg, err := bufio.NewReader(c.conn).ReadString('\n')
+		if err != nil {
+			return
+		}
+
+		msg = strings.Trim(msg, "\r\n")
+		args := strings.Split(msg, " ")
+		cmd := strings.TrimSpace(args[0])
+
+		switch cmd {
+		case "/name":
+			c.commandChan <- command{
+				id:     CMD_NICK,
+				client: c,
+				args:   args,
+			}
+		case "/join":
+			c.commandChan <- command{
+				id:     CMD_JOIN,
+				client: c,
+				args:   args,
+			}
+		case "/rooms":
+			c.commandChan <- command{
+				id:     CMD_ROOMS,
+				client: c,
+				args:   args,
+			}
+		case "/msg":
+			c.commandChan <- command{
+				id:     CMD_MSG,
+				client: c,
+				args:   args,
+			}
+		case "/quit":
+			c.commandChan <- command{
+				id:     CMD_QUIT,
+				client: c,
+				args:   args,
+			}
+		default:
+			c.err(fmt.Errorf("Unknown Command %s", cmd))
+		}
+	}
+}
+
+func (c *client) err(err error) {
+	c.conn.Write([]byte("Error: " + err.Error() + "\n"))
+}
+
+func (c *client) msg(msg string) {
+	c.conn.Write([]byte(">> " + msg + "\n"))
 }
