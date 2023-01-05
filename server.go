e4d6ca0ca5a5552a9ceabaa209945e0772faaf86
Gagan Gowda <gagangowda26@gmail.com>
2023-01-04 19:12:12 -0800

    Defined functions for name and joining rooms

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 )
@@ -23,5 +24,70 @@ func (s *server) newClient(conn net.Conn) *client {
 		conn:        conn,
 		name:        "anonymous",
 		commandChan: s.commandChan,
+	}
+}
+
+func (s *server) run() {
+	for cmd := range s.commandChan {
+		switch cmd.id {
+		case CMD_NICK:
+			s.name(cmd.client, cmd.args)
+		case CMD_JOIN:
+			s.join(cmd.client, cmd.args)
+		case CMD_ROOMS:
+			s.currentRooms(cmd.client, cmd.args)
+		case CMD_MSG:
+			s.msg(cmd.client, cmd.args)
+		case CMD_QUIT:
+			s.quitRoom(cmd.client, cmd.args)
+		}
+	}
+
+}
+
+func (s *server) name(c *client, args []string) {
+	name := args[1]
+	c.name = name
+	c.msg(fmt.Sprintf("Let's call you %s", name))
+}
+
+func (s *server) join(c *client, args []string) {
+	roomName := args[1]
+
+	r, ok := s.rooms[roomName]
+	if !ok {
+		r := &room{
+			name:    roomName,
+			members: make(map[net.Addr]*client),
+		}
+		s.rooms[roomName] = r
+	}
+
+	r.members[c.conn.RemoteAddr()] = c
+
+	s.quitCurrentRoom(c)
+
+	c.room = r
+
+	r.broadcast(c, fmt.Sprintf("%s has joined the room", c.name))
+	c.msg(fmt.Sprintf("Welcome to %s ", r.name))
+}
+
+func (s *server) currentRooms(c *client, args []string) {
+
+}
+
+func (s *server) msg(c *client, args []string) {
+
+}
+
+func (s *server) quitRoom(c *client, args []string) {
+
+}
+
+func (s *server) quitCurrentRoom(c *client) {
+	if c.room != nil {
+		delete(c.room.members, c.conn.RemoteAddr())
+		c.room.broadcast(c, fmt.Sprintf("%s left the room", c.name))
 	}
 }
