31653dfdeebc42137cb7c355ae8c15044f93acfc
Gagan Gowda <gagangowda26@gmail.com>
2023-01-04 19:13:35 -0800

    Added broadcast function for room struct

--- room.go
+++ room.go
@@ -6,3 +6,11 @@ type room struct {
 	name    string
 	members map[net.Addr]*client
 }
+
+func (r *room) broadcast(sender *client, msg string) {
+	for adr, c := range r.members {
+		if adr != sender.conn.RemoteAddr() {
+			c.msg(msg)
+		}
+	}
+}
